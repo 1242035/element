ba00cfcd212c961b65ff849d18f61d19fbbc2d99
Phạm Văn Điệp <diep.job@outlook.com>
2019-06-12 10:51:12 +0700

    optimize small size for jwork

--- build/bin/build-entry.js
+++ build/bin/build-entry.js
@@ -36,12 +36,7 @@ const install = function(Vue, opts = {}) {
   };
 
   Vue.prototype.$loading = Loading.service;
-  Vue.prototype.$msgbox = MessageBox;
-  Vue.prototype.$alert = MessageBox.alert;
-  Vue.prototype.$confirm = MessageBox.confirm;
-  Vue.prototype.$prompt = MessageBox.prompt;
   Vue.prototype.$notify = Notification;
-  Vue.prototype.$message = Message;
 
 };
 
